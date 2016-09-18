05457fe1488712983e42b9af43140119cd3d0c2d
vivaxy <xyxuye2007@126.com>
2016-09-18 13:05:08 +0800

    fix replaceReducer

--- source/library/store.js
+++ source/library/store.js
@@ -9,11 +9,7 @@ import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 
 import reducer from '../reducer';
 
-const combinedReducers = combineReducers({
-    ...reducer
-});
-
-const store = createStore(combinedReducers, compose(
+const store = createStore(combineReducers(reducer), compose(
     applyMiddleware(),
     window.devToolsExtension ? window.devToolsExtension() : f => f
 ));
@@ -22,7 +18,7 @@ if (module.hot) {
     // Enable Webpack hot module replacement for reducers
     module.hot.accept('../reducer', () => {
         const nextRootReducer = require('../reducer').default;
-        store.replaceReducer(nextRootReducer);
+        store.replaceReducer(combineReducers(nextRootReducer));
     });
 }
 
